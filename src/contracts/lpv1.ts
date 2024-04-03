77185b484d43532cc2a22e3bb5582e6d7e37e50b
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2024-04-03 22:34:03 +0200

    more core

--- src/contracts/lpv1.ts
+++ src/contracts/lpv1.ts
@@ -34,7 +34,7 @@ export async function getTokenForPool(
     const address = await Promise.resolve(lpv1token.methods.token().call());
 
     const entry = {
-      token: address?.toString() || "",
+      token: address?.toString().toLowerCase() || "",
     } as const;
 
     cache.add({ [contractAddress.toLowerCase()]: entry as any });
@@ -43,5 +43,44 @@ export async function getTokenForPool(
   } catch (err) {
     console.error("An error occurred", err);
     throw err; // or handle the error as needed
+  }
+}
+
+
+export async function getAzuroBetAddress(
+  contractAddress: string,
+  chainId: number
+): Promise<{
+  readonly azuroBetAddress: string;
+}> {
+  const cache = Cache.init(CacheCategory.LPv1Bet, chainId);
+  const lpv1 = cache.read(contractAddress.toLowerCase());
+
+  if (lpv1) {
+    return lpv1;
+  }
+
+  // RPC URL
+  const rpcURL = CHAIN_CONSTANTS[chainId].rpcURL;
+
+  // Create Web3 instance
+  const web3 = new Web3(rpcURL);
+
+  // Create LPv1 contract instance
+  const _lpv1 = new web3.eth.Contract(contractABI, contractAddress);
+
+  try {
+    const azuroBetAddress = await Promise.resolve(_lpv1.methods.azuroBet().call());
+
+    const entry = {
+      azuroBet: azuroBetAddress?.toString().toLowerCase() || "",
+    } as const;
+
+    cache.add({ [contractAddress.toLowerCase()]: entry as any });
+
+    return entry;
+  } catch (err) {
+    console.error("An error occurred", err);
+    throw err
   }
 }
