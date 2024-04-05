b114369715c99267fbddc2448d8e38f110279648
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2024-04-06 00:29:38 +0200

    factory v2

---
+++ src/whitelists.ts
@@ -0,0 +1,40 @@
+export const LP_WHITELIST = [
+    // gnosis xdai dev
+    '0xe068bf88317fa2eb3eaecbfe1e486d8b2dde7761',
+    // mumbai usdc dev
+    '0xbaeb482a9933168d0708d9c8f6f6dc056994e35e',
+    // gnosis xdai prod
+    '0x204e7371ade792c5c006fb52711c50a7efc843ed',
+    // polygon usdc prod
+    '0x2a838ab9b037db117576db8d0dcc3b686748ef7c',
+    // mumbai usdt dev
+    '0xe47f16bc95f4cf421f008bc5c23c1d3d5f402935',
+    // polygon usdt prod
+    '0x7043e4e1c4045424858ecbced80989feafc11b36',
+    // arbitrum goerli usdt dev
+    '0x482e419711e63d0e49cbdc696858ef1e4764771e',
+    // arbitrum one usdt prod
+    '0x20513ba6a4717c67e14291331bc99dd2ace90038',
+    // linea goerli usdt dev / linea usdt prod
+    '0xc365224ef4fa75d56a280c5a3925cadbf7bd8eee',
+    // v2.8.7
+    // gnosis xdai dev
+    '0x7f227c55a78d6fcdc6aad982059e43075a282d38',
+    // mumbai usdt dev
+    '0x47c81b04a55346e724fdc3fec8e0628e8a1f11fc',
+    // arbitrum goerli usdt dev
+    '0x4ac2f9cbfeb932cc5c10f6701dc5a5f157cf5246',
+    // v2.9.0
+    // gnosis xdai dev
+    '0x1b31ee571db0dde82c4734854b8adbb87b100c44',
+    // v2.10.0
+    // gnosis xdai dev
+    '0x4e5bbe3e1f559c8010b516c603b619a932de3df4',
+    // mumbai usdt dev
+    '0xe511677660402c9e1741510b811906e5e905f003',
+    // arbitrum goerli usdt dev
+    '0x02a88e08142b262cc66c81977d8fbe86afe90c58',
+    // mumbai azusd prod
+    '0xa39e988b357122d9ba75c0038e625a5ab9363f54',
+  ]
+  
