eae0d2ff07e7c337e45249f25654ec8c72801ac5
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2024-04-09 08:42:55 +0200

    uncomment v1

--- src/v1/azurobets.ts
+++ src/v1/azurobets.ts
@@ -9,13 +9,13 @@ AzurobetsContract_Transfer_loader(({ event, context }) => {
   context.AzuroBetContract.load(event.srcAddress, {});
 });
 AzurobetsContract_Transfer_handler(({ event, context }) => {
-  // transferBet(
-  //   null,
-  //   event.srcAddress,
-  //   event.params.tokenId,
-  //   event.params.from,
-  //   event.params.to,
-  //   event.blockNumber,
-  //   context
-  // )
+  transferBet(
+    null,
+    event.srcAddress,
+    event.params.tokenId,
+    event.params.from,
+    event.params.to,
+    event.blockNumber,
+    context
+  )
 });
